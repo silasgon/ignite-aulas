c33f4a9d08c8c9902af3a3efc1792511e8a09bd3
Silas Gonm <silasidc@gmail.com>
2022-03-28 18:33:40 -0300

    desevolvendo os requisitos de deposito

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -1,3 +1,4 @@
+const { request, response } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
@@ -15,7 +16,19 @@ const customers = [];
 */
 
 //Middleaware
+function verifyIfExistsAccountCPF(request, response, next){
+    const { cpf } = request.headers;
 
+    const customer = customers.find((customer) => customer.cpf === cpf);
+    
+    if(!customer){
+        return response.status(400).json({ error: "Customer not found!"});
+    }
+
+    request.customer = customer;
+
+    return next();
+}
 
 app.post('/account', (req, res)=> {
     const { cpf, name } = req.body;
@@ -35,21 +48,35 @@ app.post('/account', (req, res)=> {
         statement: []
     });
     
-    console.log(customers)
+   // console.log(customers)
     return res.status(201).send();
 });
 
-app.get('/statement ', (req, res) => {
-    const { cpf } = req.headers;
+//Usando midleware para varias rotas a seguir
+//app.use(verifyIfExistsAccountCPF);
 
-    const customer = customers.find((customer) => customer.cpf === cpf);
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
 
-    if(!customer){
-        return res.status(400).json({ error: "Customer not found!"})
-    }
-
-    return res.json(customer.statement);
+    console.log(customer);
+    return response.json(customer.statement);
 });
 
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
 app.listen(3333);
 
