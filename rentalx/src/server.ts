d441bdf6def78acb2132ce38d9b7f38e6b7c2a54
Silas Gonm <silasidc@gmail.com>
2022-04-12 18:34:47 -0300

    routes

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -4,14 +4,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-    return response.json({ message: "Hello Word SLS"});
-});
-
-app.post('/courses', (request, response) => {
-    const {name} = request.body;
-    return response.json({ name });
-});
 
 app.listen(3333, () => console.log("Server is running"));
 
