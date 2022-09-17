1b9cbe1dc855d7774035e6d149fd75941d2cc53a
Silas Gon <silasidc@gmail.com>
2022-09-17 17:23:48 -0300

    nlw class 2 - intro node, react & reactNative

---
+++ nlw/esports/mobile/App.tsx
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      <Button title='Send 1'/>
+      <Button title='Send 2'/>
+      <Button title='Send 3'/>
+      <Button title='Hello world'/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+interface ButtonProps{
+  title: string;
+}
+
+function Button(props: ButtonProps){
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
