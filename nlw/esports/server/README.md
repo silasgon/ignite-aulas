fee9fadd88f76c5094208839976bd15523cc63c5
Silas Gon <silasidc@gmail.com>
2022-09-21 00:21:31 -0300

    starting routes API

---
+++ nlw/esports/server/README.md
@@ -0,0 +1,34 @@
+# BackEnd
+
+## Entities
+
+### Game
+
+id
+title
+bannerUrl
+
+### Ads
+
+id
+gameId
+name
+yearsPlaying
+discord
+weekDays
+hourStart
+hourEnd
+useVoiceChannel
+createdAt
+
+1:30 -> 90 min
+R$ 179,89 -> (*100 <-> /100) 17989
+
+Datas (fuso horario / formatos diferentes i18n)
+
+## Use Cases
+- Listagem de games com contagens de anúncios
+- Criação de novo anúncio
+- Listagem de anúncios por game
+- Buscar discord pelo ID do anúncio
+
--- nlw/esports/server/src/server.ts
+++ nlw/esports/server/src/server.ts
@@ -1,15 +1,33 @@
-import express from "express";
+import express, { request, response } from "express";
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+    return response.json([
+        {id: 1, name: 'Dota 2'},
+        {id: 2, name: 'Dota Reborn'},
+        {id: 3, name: 'Counter Strike'},
+    ]);
+});
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json(['ads']);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+    const gameId = request.params.id;
+
     return response.json([
         {id: 1, name: 'Anúncio 1'},
         {id: 2, name: 'Anúncio 2'},
         {id: 3, name: 'Anúncio 3'},
-        {id: 4, name: 'Anúncio 4'},
-        {id: 5, name: 'Anúncio 5'},
     ])
-})
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+    //const adId = request.params.id;
+    
+    return response.status(200).json([])
+});
 
 app.listen(3333)
